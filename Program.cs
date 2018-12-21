37b6588e93e055fc55852efc384998765865aefe
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-21 11:28:25 +0100

    corrction

--- Program.cs
+++ Program.cs
@@ -10,16 +10,14 @@ namespace TP01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bonjour, ce programme va calculer votre IMC (Indice de Masse Corporelle)");
-            Console.WriteLine("Entrez votre taille en m ( exemple : 1,70)");
-            double height = double.Parse(Console.ReadLine());
+            Console.WriteLine("Bonjour, ce programme va calculer votre IMC (Indice de Masse Corporelle) \n");
+            Console.WriteLine("Entrez votre taille en m (exemple : 1,70)");
+            double height = double.Parse(Console.ReadLine()); // on affecte valeur récupéré convertie en double à la var "height"
             Console.WriteLine("Entrez votre poids en kg");
-            double weight = double.Parse(Console.ReadLine());
-             
-            double imc = (weight / Math.Pow(height,2));
-
+            double weight = double.Parse(Console.ReadLine()); // idem avec "weight"
+            double imc = Math.Round(weight / Math.Pow(height,2),2); // calcul de l'IMC : poids / taille au carré
             Console.WriteLine($"Votre Indice de Masse Corporelle est : {imc}");
-
+            // selon la valeur de l'IMC, on obtient un message adapté
             if (imc < 16.5)
             {
                 Console.WriteLine("Dénutrition");
@@ -50,11 +48,8 @@ namespace TP01
             }
             else
             {
-                Console.WriteLine("Je n'ai pas compris !");
+                Console.WriteLine("Je n'ai pas compris ! Il doit y avoir une erreur...");
             }
-            
-
-
         }
     }
 }
